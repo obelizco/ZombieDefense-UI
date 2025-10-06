29ea1d32e8ebd322219a482c81f0093c7c2e68ce
Omar Carreno <omaraguirre10@gmail.com>
2025-10-05 23:29:20 -0500

    Ajustando enviroment de api en azure

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7259/defense',
+  apiUrl: 'https://zombiedefenseapi-gvanb2ctcfbsc8aa.centralus-01.azurewebsites.net/defense',
   x_apy_key:'ZombieApiKEY'
 };
